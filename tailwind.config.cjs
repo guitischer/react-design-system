1722bd8dc196a9053b6682e63c4286e7eb1e58c6
guitischer <guitischer@hotmail.com.br>
2022-10-12 21:25:16 -0300

    configure tailwind colors and font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,24 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      fontSize: {
+        xs: 14,
+        sm: 16,
+        md: 18,
+        lg: 20,
+        xl: 24,
+        "2xl": 32,
+      },
+      colors: {
+        "grey-900": "#121214",
+        "grey-800": "#202024",
+        "grey-400": "#7C7C8A",
+        "grey-200": "#C4C4CC",
+        "grey-100": "#E1E1E6",
+
+        "cian-300": "#9BE1FB",
+        "cian-500": "#81D8F7",
+      },
       fontFamily: {
         sans: "Inter, sans-serif",
       },
